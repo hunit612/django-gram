ab944f49c39472d9d67ec89d333bc28cc433f7c1
hunit <hunit612@gmail.com>
2021-02-08 17:47:50 +0900

    create Loginapi

--- apis/views.py
+++ apis/views.py
@@ -1,5 +1,11 @@
 from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from django.views import View
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.core.validators import validate_email, ValidationError
+from djanog.contrib.auth import authenticate, login
 
 
 class BaseView(View):
@@ -13,15 +19,43 @@ class BaseView(View):
 
 
 class UserCreateView(BaseView):
-    @method-decorator(csrf_exempt)
+    @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super(UserCreateView, self).dispatch(request, *args, **kwargs)
 
     def post(self, request):
         username = request.POST.get('username', '')
+        if not username:
+            return self.response(message='아이디를 입력해주세요.', status=400)
         password = request.POST.get('password', '')
+        if not password:
+            return self.response(message='패스워드를 입력해주세요.', status=400)
         email = request.POST.get('email', '')
+        try:
+            validate_email(email)
+        except ValidationError:
+            self.response(message='올바른 이메일을 입력해주세요.', status=400)
 
-        user = User.objects.create_user(user, email, password)
+        try:
+            user = User.objects.create_user(username, email, password)
+        except IntegrityError:
+            return self.response(message='이미 존재하는 아이디입니다.', status=400)
 
         return self.response({'user.id':user.id})
+
+
+class UserLoginView(BaseView):
+    def post(self, request):
+        username = request.POST.get('username', '')
+        if not username:
+            return self.response(message='아이디를 입력해주세요.', status=400)
+        password = request.POST.get('password', '')
+        if not password:
+            return self.response(message='비밀번호를 입력해주세요.' status=400)
+
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            return self.response(message='입력정보를 확인해주세요.', status=400)
+        login(request, user)
+
+        return self.response
